5298cab3fbd0de63690f2298de4b1edff3249a2f
ThorGalli <gallithor@gmail.com>
2024-10-25 11:36:46 -0300

    Remove some comments

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from coin_fetcher import fetch_prices
 
 
 def fetch_coins():
-    fetch_index = 0  # Initialize fetch index
+    fetch_index = 0
     while True:
-        fetch_index += 1  # Increment fetch index
+        fetch_index += 1
         print(f"\nStarting fetch #{fetch_index}...")
         fetch_prices()
-        print(f"Fetch #{fetch_index} complete. Waiting for 60 seconds...\n")
-        time.sleep(10)  # Wait for 60 seconds before the next fetch
+        print(f"Fetch #{fetch_index} complete. Waiting for 10 seconds...\n")
+        time.sleep(10)
 
 
 if __name__ == "__main__":
